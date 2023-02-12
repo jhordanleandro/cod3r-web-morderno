3617e53de16709492f2649301d7615a864642398
Jhordan Leandro <jhordanleandro@hotmail.com>
2023-02-12 18:33:18 -0300

    enviando os primeiros arquivos

---
+++ 1.fundamentos/1.1.organizacao.js
@@ -0,0 +1,4 @@
+console.log("Sentença de código");
+
+//bloco de códigos são delimitados por {};
+
---
+++ 1.fundamentos/1.3.variaveisConstantes.js
@@ -0,0 +1,3 @@
+var a = 3; // o simbolo = atribui um valor à variável;
+let b = 4; // foma mais moderna para declarar as variáveis;
+const c = 5; // forma para declaração de constantes.
