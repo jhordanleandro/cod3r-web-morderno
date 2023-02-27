a93cdc8da5ce19cd46f87cc0f4fb0279b3ded3b3
Jhordan Leandro <jhordanleandro@hotmail.com>
2023-02-26 21:21:02 -0300

    novos arquivos

---
+++ 1.fundamentos/1.16.usandoVar1.js
@@ -0,0 +1,19 @@
+{
+  {
+    {
+      {
+        var sera = "Será???";
+        console.log(sera);
+      }
+    }
+  }
+}
+console.log(sera);
+
+function teste() {
+  var local = 123;
+  console.log(local);
+}
+
+teste();
+//console.log(local); A variável local fica acessível apenas dentro da função!
---
+++ 1.fundamentos/1.20.usandoLetEmLoop1.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log("i =", i); //neste caso o i não será impresso por está atribuido com let quem tem escopo de bloco;
